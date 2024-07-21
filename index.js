e5b52efa2649b923c8780bb874ca653e7dba076e
brendan544 <brendan.gwer@student.moringaschool.com>
2024-07-21 22:06:29 +0300

    message

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  if (distanceInFeet <= 400) {
+    return "This one is on me!";
+} else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+} else if (distanceInFeet > 2500) {
+    return "No can do.";
+} else {
+    return "Please enter a valid distance.";
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  return (destination === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+        return 'Thank you so much.';
+    case 'regular':
+        return 'Thank you.';
+    default:
+        return 'Bye.';
+  }
 }
